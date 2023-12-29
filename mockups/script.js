074d7b577ab5b0373be9923b353c08628b750ad6
Kodali7 <kodali.sai07@gmail.com>
2023-12-29 15:43:23 -0600

    new update

---
+++ mockups/script.js
@@ -0,0 +1,41 @@
+
+
+$('#s4yt-what').click(function(){
+    $('#txt1').toggle();
+    $('#txt2').hide();
+    $('#txt3').hide();
+    $('#txt4').hide();
+    $('#txt5').hide();
+})
+
+$('#s4yt-ami').click(function(){
+    $('#txt1').hide();
+    $('#txt2').show();
+    $('#txt3').hide();
+    $('#txt4').hide();
+    $('#txt5').hide();
+})
+
+$('#s4yt-when').click(function(){
+    $('#txt1').hide();
+    $('#txt2').hide();
+    $('#txt3').show();
+    $('#txt4').hide();
+    $('#txt5').hide();
+})
+
+$('#s4yt-howdo').click(function(){
+    $('#txt1').hide();
+    $('#txt2').hide();
+    $('#txt3').hide();
+    $('#txt4').show();
+    $('#txt5').hide();
+})
+
+$('#s4yt-whatare').click(function(){
+    $('#txt1').hide();
+    $('#txt2').hide();
+    $('#txt3').hide();
+    $('#txt4').hide();
+    $('#txt5').show();
+})
